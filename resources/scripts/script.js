928baebf81204effac3b7152c959d37612f6dd3a
telepath01 <47895030+telepath01@users.noreply.github.com>
2023-01-02 22:52:32 -0500

    working on the script
    working on the js file.  The look is complete

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,2 +1,45 @@
 "use strict";
 
+// HTML Refrences
+const bookAddBtn = document.querySelector(".add-book");
+const formContainer = document.querySelector(".book-form-container");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const readInputYes = document.getElementById("yes");
+const readInputNo = document.getElementById("no");
+const submitBtn = document.querySelector(".submit-btn");
+const bookParent = document.getElementById("book-main");
+// Element creation
+const createDiv = document.createElement("div");
+
+// Global Variables
+let formActive = false;
+let myLibrary = [
+  {
+    title: "Doom",
+    author: "Paul",
+    pages: 300,
+    read: true,
+  },
+];
+
+// Constructors
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+// Fucntions
+
+function createBook(book, index) {
+  createDiv;
+}
+
+function bookRender() {
+  myLibrary.map((book, index) => {
+    createBook(book, index);
+  });
+}
+bookRender();
