c8a817a139330cef6b3a9e1bf56adab384a33b16
telepath01 <47895030+telepath01@users.noreply.github.com>
2023-01-03 22:19:46 -0500

    Script update
    working on the script portion , need to figure out how to create the refresh function within itself

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -10,19 +10,17 @@ const readInputYes = document.getElementById("yes");
 const readInputNo = document.getElementById("no");
 const submitBtn = document.querySelector(".submit-btn");
 const bookParent = document.getElementById("book-main");
-// Element creation
-const createDiv = document.createElement("div");
 
+// Element creation
+let deleteButtons;
 // Global Variables
+const editImage = "resources/images/pencil.png";
+const deleteImage = "resources/images/erase.png";
 let formActive = false;
-let myLibrary = [
-  {
-    title: "Doom",
-    author: "Paul",
-    pages: 300,
-    read: true,
-  },
-];
+let book = {};
+let myLibrary = [];
+let read = "";
+let currentDiv = "";
 
 // Constructors
 function Book(title, author, pages, read) {
@@ -32,9 +30,73 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 // Fucntions
+function vallueReset() {
+  titleInput.value = "";
+}
+
+function checkRead() {
+  if (readInputNo.checked) {
+    read = "No";
+  } else if (readInputYes.checked) {
+    read = "Yes";
+  }
+}
+
+function creatBookSections(el, content, className) {
+  const element = document.createElement(el);
+  element.textContent = content;
+  element.setAttribute("class", className);
+  return element;
+}
+function createImage(image) {
+  const buttonImage = document.createElement("img");
+  buttonImage.setAttribute("src", image);
+  buttonImage.setAttribute("class", "block-img");
+  return buttonImage;
+}
+
+function createButtonSection(el, content, className) {
+  const creatButton = document.createElement(el);
+  creatButton.appendChild(content);
+  creatButton.setAttribute("class", className);
+  return creatButton;
+}
 
 function createBook(book, index) {
-  createDiv;
+  const createDiv = document.createElement("div");
+  const createDivChild = document.createElement("div");
+  const createDivButton = document.createElement("div");
+  bookParent.appendChild(createDiv);
+  createDiv.setAttribute("id", index);
+  createDiv.setAttribute("key", index);
+  createDiv.classList.add("book-block");
+  createDiv.appendChild(createDivChild);
+  createDivChild.setAttribute("id", index);
+  createDivChild.setAttribute("key", index);
+  createDivChild.classList.add("card-body");
+  createDivChild.appendChild(
+    creatBookSections("h3", `Title:${book.title}`, "book-title")
+  );
+  createDivChild.appendChild(
+    creatBookSections("p", `Author:${book.author}`, "book-author")
+  );
+  createDivChild.appendChild(
+    creatBookSections("p", `Pages:${book.pages}`, "book-pages")
+  );
+  createDivChild.appendChild(
+    creatBookSections("p", `Read:${book.read}`, "book-read")
+  );
+  createDiv.appendChild(createDivButton);
+  createDivButton.setAttribute("id", index);
+  createDivButton.setAttribute("key", index);
+  createDivButton.classList.add("card-buttons");
+  createDivButton.appendChild(
+    createButtonSection("button", createImage(editImage), "edit")
+  );
+  createDivButton.appendChild(
+    createButtonSection("button", createImage(deleteImage), "delete")
+  );
+  deleteButtons = document.querySelectorAll(".delete");
 }
 
 function bookRender() {
@@ -42,4 +104,25 @@ function bookRender() {
     createBook(book, index);
   });
 }
-bookRender();
+
+bookAddBtn.addEventListener("click", () => {
+  if (!formActive) {
+    formContainer.style.display = "grid";
+    formActive = true;
+  } else if (formActive) {
+    formContainer.style.display = "none";
+    formActive = false;
+  }
+});
+
+submitBtn.addEventListener("click", () => {
+  checkRead();
+  book = new Book(titleInput.value, authorInput.value, pagesInput.value, read);
+  myLibrary.push(book);
+  // bookRender();
+  // addBook();
+  book = {};
+  vallueReset();
+  formContainer.style.display = "none";
+  event.preventDefault();
+});
