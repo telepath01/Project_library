9ae64277d30b8d0bd2efcee1dda7e48ae49d6734
telepath01 <47895030+telepath01@users.noreply.github.com>
2023-01-13 08:57:09 -0500

    Constructor to Class Book
    Changed the Book Constructor into a Class Book. Checked the code to see if works and all is good.

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -23,12 +23,16 @@ let read = "";
 let currentDiv = "";
 
 // Constructors
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
+
+
 // Fucntions
 function vallueReset() {
   titleInput.value = "";
